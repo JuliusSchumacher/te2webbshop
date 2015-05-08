cc91d2fd375dc4243f5b45ed1ba9e9a0b7211fb5
mranstrand <michael.ranstrand@gmail.com>
2015-05-08 14:10:48 +0200

    Rgeisterformulär klart

---
+++ registrera_produkt_form.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>Registrera ny produkt</title>
+</head>
+<body>
+<h1>Registrera ny produkt</h1><br><br>
+
+<form action=".php" method="post">
+    Titel <input type="text" name="titel"><br><br>
+    Beskrivning <input type="text" name="beskrivning"><br><br>
+    Pris <input type="text" name="pris"><br><br>
+    Lagersaldo <input type="text" name="lagersaldo"><br><br>
+    URL till bild <input type="text" name="bildfil"><br><br>
+    <input type="submit">
+</form>
+</body>
+</html>
